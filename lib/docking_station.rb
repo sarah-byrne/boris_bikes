b1fa9046ec1d8f7e382daa31044496d27cd3b585
Student <student@Admins-MacBook-Pro.local>
2019-02-06 17:11:45 +0000

    add code for docking a bike and attr reader for showing docked bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,11 +1,17 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :boris_bikes
+
+  def initialize
+    @boris_bikes = []
+  end
 
   def release_bike
     Bike.new
   end
 
   def dock(bike)
+    @boris_bikes.push(bike)
   end
 end
