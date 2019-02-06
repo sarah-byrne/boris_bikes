2a82f79e58e8517b8a7cec8bc16652a55a216f5d
Student <student@Admins-MacBook-Pro.local>
2019-02-06 15:27:09 +0000

    add a test that DockingStation can release a working bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,13 @@
 require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike}
+
+  it 'can release a working bike' do
+    # Arrange
+    docking_station = DockingStation.new
+    # Act
+    bike = docking_station.release_bike
+    # Assert
+    expect(bike.working?).to eq(true)
+  end
 end
