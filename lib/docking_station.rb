0f035a0028d6561433d3b456876389732ec54077
Clare Pinder <Clare.pinder@live.co.uk>
2019-02-07 09:51:28 +0000

    Step 11 with code refactored

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,22 +4,13 @@ class DockingStation
   attr_reader :bike
 
   def initialize
-    # @bikes = []
   end
 
   def release_bike
-    # bikes.last.delete
     Bike.new
-    # remove bike from array bikes.delete_if ....
   end
 
   def dock(bike)
     @bike = bike
-  #   bikes.push(bike)
   end
-
-  def bike
-    @bike
-  end
-
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,7 +4,7 @@ require 'docking_station'
 describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
-  # it { is_expected.to respond_to(:dock).with(1).argument }
+  it { is_expected.to respond_to(:dock).with(1).argument }
   it 'docks something' do
     bike = Bike.new
     expect(subject.dock(bike)).to eq bike
